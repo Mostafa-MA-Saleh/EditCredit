bc84dd07853556e236f3cb2384b2c67e9180c6dd
Mostafa Saleh <Mostafa.MA.Saleh@gmail.com>
2018-05-20 03:47:38 +0200

    - Updated the demo app to include the gravity options.

--- app/src/main/java/mostafa/ma/saleh/gmail/com/editcreditdemo/MainActivity.java
+++ app/src/main/java/mostafa/ma/saleh/gmail/com/editcreditdemo/MainActivity.java
@@ -10,17 +10,21 @@ import android.widget.CompoundButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import mostafa.ma.saleh.gmail.com.editcredit.EditCredit;
 
 public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener, View.OnClickListener, CheckBox.OnCheckedChangeListener {
 
     private EditCredit mEditCredit;
-    private RadioGroup rgOptions;
-    private Button btnValidate;
-    private Button btnGetNumber;
-    private CheckBox chkVisa;
-    private CheckBox chkMasterCard;
-    private CheckBox chkAMEX;
+    private RadioGroup separatorsRadioGroup;
+    private RadioGroup gravityRadioGroup;
+    private Button validateButton;
+    private Button getNumberButton;
+    private CheckBox visaCheckBox;
+    private CheckBox masterCardCheckBox;
+    private CheckBox americanExpressCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,57 +35,87 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
     }
 
     private void setListeners() {
-        rgOptions.setOnCheckedChangeListener(this);
-        btnValidate.setOnClickListener(this);
-        btnGetNumber.setOnClickListener(this);
-        chkVisa.setOnCheckedChangeListener(this);
-        chkMasterCard.setOnCheckedChangeListener(this);
-        chkAMEX.setOnCheckedChangeListener(this);
+        separatorsRadioGroup.setOnCheckedChangeListener(this);
+        gravityRadioGroup.setOnCheckedChangeListener(this);
+        validateButton.setOnClickListener(this);
+        getNumberButton.setOnClickListener(this);
+        visaCheckBox.setOnCheckedChangeListener(this);
+        masterCardCheckBox.setOnCheckedChangeListener(this);
+        americanExpressCheckBox.setOnCheckedChangeListener(this);
     }
 
-    private void findViewsById(){
-        mEditCredit = (EditCredit) findViewById(R.id.editcredit);
-        rgOptions = (RadioGroup) findViewById(R.id.rg_options);
-        btnValidate = (Button) findViewById(R.id.btn_validate);
-        btnGetNumber = (Button) findViewById(R.id.btn_get_number);
-        chkVisa = (CheckBox) findViewById(R.id.chk_visa);
-        chkMasterCard = (CheckBox) findViewById(R.id.chk_mastercard);
-        chkAMEX = (CheckBox) findViewById(R.id.chk_amex);
+    private void findViewsById() {
+        mEditCredit = findViewById(R.id.editcredit);
+        separatorsRadioGroup = findViewById(R.id.separators_radio_group);
+        gravityRadioGroup = findViewById(R.id.gravity_radio_group);
+        validateButton = findViewById(R.id.btn_validate);
+        getNumberButton = findViewById(R.id.btn_get_number);
+        visaCheckBox = findViewById(R.id.chk_visa);
+        masterCardCheckBox = findViewById(R.id.chk_mastercard);
+        americanExpressCheckBox = findViewById(R.id.chk_amex);
     }
 
     @Override
     public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
-        switch (checkedId){
-            case R.id.rb_no_separators:
-                mEditCredit.setSeparator(EditCredit.NO_SEPARATOR);
+        switch (group.getId()) {
+            case R.id.separators_radio_group:
+                onSeparatorsCheckedChanged(checkedId);
                 break;
-            case R.id.rb_spaces:
-                mEditCredit.setSeparator(EditCredit.SPACES_SEPARATOR);
+            case R.id.gravity_radio_group:
+                onGravityCheckedChanged(checkedId);
                 break;
-            case R.id.rb_dashes:
-                mEditCredit.setSeparator(EditCredit.DASHES_SEPARATOR);
+        }
+    }
+
+    private void onSeparatorsCheckedChanged(@IdRes int checkedId) {
+        switch (checkedId) {
+            case R.id.no_separators_radio_button:
+                mEditCredit.setSeparator(EditCredit.Separator.NONE);
+                break;
+            case R.id.spaces_radio_button:
+                mEditCredit.setSeparator(EditCredit.Separator.SPACES);
+                break;
+            case R.id.dashes_radio_button:
+                mEditCredit.setSeparator(EditCredit.Separator.DASHES);
+                break;
+        }
+    }
+
+    private void onGravityCheckedChanged(@IdRes int checkedId) {
+        switch (checkedId) {
+            case R.id.start_radio_button:
+                mEditCredit.setDrawableGravity(EditCredit.Gravity.START);
+                break;
+            case R.id.end_radio_button:
+                mEditCredit.setDrawableGravity(EditCredit.Gravity.END);
+                break;
+            case R.id.left_radio_button:
+                mEditCredit.setDrawableGravity(EditCredit.Gravity.LEFT);
+                break;
+            case R.id.right_radio_button:
+                mEditCredit.setDrawableGravity(EditCredit.Gravity.RIGHT);
                 break;
         }
     }
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        int disabledCards = EditCredit.NONE;
-        if(chkVisa.isChecked()){
-            disabledCards |= EditCredit.VISA;
+        List<EditCredit.Card> disabledCards = new ArrayList<>();
+        if (visaCheckBox.isChecked()) {
+            disabledCards.add(EditCredit.Card.VISA);
         }
-        if(chkMasterCard.isChecked()){
-            disabledCards |= EditCredit.MASTERCARD;
+        if (masterCardCheckBox.isChecked()) {
+            disabledCards.add(EditCredit.Card.MASTERCARD);
         }
-        if (chkAMEX.isChecked()){
-            disabledCards |= EditCredit.AMEX;
+        if (americanExpressCheckBox.isChecked()) {
+            disabledCards.add(EditCredit.Card.AMEX);
         }
-        mEditCredit.setDisabledCards(disabledCards);
+        mEditCredit.setDisabledCards(disabledCards.toArray(new EditCredit.Card[0]));
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn_validate:
                 Toast.makeText(this, mEditCredit.isCardValid() ? "Valid" : "Not Valid", Toast.LENGTH_SHORT).show();
                 break;
@@ -90,6 +124,4 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
                 break;
         }
     }
-
-
 }
