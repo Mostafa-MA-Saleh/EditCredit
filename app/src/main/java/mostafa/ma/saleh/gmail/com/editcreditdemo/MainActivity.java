53299849cce783ab094e794cda06b6f073025b6d
Mostafa Saleh <mostafa.saleh@bugzy.net>
2018-11-04 00:54:06 +0200

    - Migrated to AndroidX.

--- app/src/main/java/mostafa/ma/saleh/gmail/com/editcreditdemo/MainActivity.java
+++ app/src/main/java/mostafa/ma/saleh/gmail/com/editcreditdemo/MainActivity.java
@@ -1,8 +1,8 @@
 package mostafa.ma.saleh.gmail.com.editcreditdemo;
 
 import android.os.Bundle;
-import android.support.annotation.IdRes;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.IdRes;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
