237f6d73879b10cdea5e6ce82eb1cc283cfd79e2
Mostafa Saleh <mostafa.saleh@bugzy.net>
2018-11-04 21:41:02 +0200

    - Rewrote the library in kotlin. - Increased the version name.

---
+++ app/src/main/java/mostafa/ma/saleh/gmail/com/editcreditdemo/MainActivity.kt
@@ -0,0 +1,87 @@
+package mostafa.ma.saleh.gmail.com.editcreditdemo
+
+import android.os.Bundle
+import android.view.View
+import android.widget.CompoundButton
+import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.annotation.IdRes
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import mostafa.ma.saleh.gmail.com.editcredit.EditCredit
+
+class MainActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener, CompoundButton.OnCheckedChangeListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        addListeners()
+    }
+
+    private fun addListeners() {
+        separatorsRadioGroup.setOnCheckedChangeListener(this)
+        gravityRadioGroup.setOnCheckedChangeListener(this)
+        visaCheckBox.setOnCheckedChangeListener(this)
+        masterCardCheckBox.setOnCheckedChangeListener(this)
+        americanExpressCheckBox.setOnCheckedChangeListener(this)
+        discoverCheckBox.setOnCheckedChangeListener(this)
+    }
+
+    override fun onCheckedChanged(group: RadioGroup, @IdRes checkedId: Int) {
+        when (group.id) {
+            R.id.separatorsRadioGroup -> onSeparatorsCheckedChanged(checkedId)
+            R.id.gravityRadioGroup -> onGravityCheckedChanged(checkedId)
+        }
+    }
+
+    private fun onSeparatorsCheckedChanged(@IdRes checkedId: Int) =
+            editCredit.setSeparator(when (checkedId) {
+                R.id.spacesRadioButton -> EditCredit.Separator.SPACES
+                R.id.dashesRadioButton -> EditCredit.Separator.DASHES
+                else -> EditCredit.Separator.NONE
+            })
+
+    private fun onGravityCheckedChanged(@IdRes checkedId: Int) =
+            editCredit.setDrawableGravity(when (checkedId) {
+                R.id.startRadioButton -> EditCredit.Gravity.START
+                R.id.leftRadioButton -> EditCredit.Gravity.LEFT
+                R.id.rightRadioButton -> EditCredit.Gravity.RIGHT
+                else -> EditCredit.Gravity.END
+            })
+
+    override fun onCheckedChanged(buttonView: CompoundButton, isChecked: Boolean) {
+        val disabledCards = mutableListOf<EditCredit.Card>()
+        if (visaCheckBox.isChecked) {
+            disabledCards.add(EditCredit.Card.VISA)
+        }
+        if (masterCardCheckBox.isChecked) {
+            disabledCards.add(EditCredit.Card.MASTERCARD)
+        }
+        if (americanExpressCheckBox.isChecked) {
+            disabledCards.add(EditCredit.Card.AMEX)
+        }
+        if (discoverCheckBox.isChecked) {
+            disabledCards.add(EditCredit.Card.DISCOVER)
+        }
+        editCredit.setDisabledCards(*disabledCards.toTypedArray())
+    }
+
+    fun onClick(v: View) {
+        val message = when (v.id) {
+            R.id.validateButton -> if (editCredit.isCardValid) "Valid" else "Not Valid"
+            R.id.getNumberButton -> editCredit.textWithoutSeparator
+            R.id.getTypeButton -> getCardTypeString(editCredit.cardType)
+            else -> return
+        }
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getCardTypeString(card: EditCredit.Card) =
+            getString(when (card) {
+                EditCredit.Card.VISA -> R.string.visa
+                EditCredit.Card.MASTERCARD -> R.string.mastercard
+                EditCredit.Card.AMEX -> R.string.american_express
+                EditCredit.Card.DISCOVER -> R.string.discover
+                else -> R.string.unknown
+            })
+}
---
+++ editcredit/src/main/java/mostafa/ma/saleh/gmail/com/editcredit/EditCredit.kt
@@ -0,0 +1,331 @@
+package mostafa.ma.saleh.gmail.com.editcredit
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.text.*
+import android.text.method.DigitsKeyListener
+import android.util.AttributeSet
+import android.util.SparseArray
+import android.widget.EditText
+import androidx.annotation.DrawableRes
+import androidx.annotation.IntRange
+import androidx.core.content.ContextCompat
+import androidx.core.widget.TextViewCompat
+import java.util.*
+import java.util.regex.Pattern
+
+class EditCredit @JvmOverloads constructor(context: Context,
+                                           attrs: AttributeSet? = null,
+                                           defStyleAttr: Int = androidx.appcompat.R.attr.editTextStyle) : EditText(context, attrs, defStyleAttr) {
+
+    private var mCCPatterns = SparseArray<Pattern>()
+    private var mSeparator: Separator = Separator.NONE
+    private var mDrawableGravity: Gravity = Gravity.END
+    private var isValidCard: Boolean = false
+    private var mCurrentDrawableResId = Card.UNKNOWN.drawableRes
+
+    val textWithoutSeparator
+        get() = if (mSeparator == Separator.NONE) {
+            text.toString()
+        } else {
+            text.toString().replace(mSeparator.toRegex(), "")
+        }
+
+    val isCardValid: Boolean
+        get() = textWithoutSeparator.length > 12 && isValidCard
+
+    val cardType: Card
+        get() = Card.from(mCurrentDrawableResId)
+
+    enum class Separator(private val stringValue: String) {
+        NONE(""), SPACES(" "), DASHES("-");
+
+        override fun toString() = stringValue
+
+        internal fun toRegex() = stringValue.toRegex()
+
+        internal val length
+            get() = stringValue.length
+    }
+
+    enum class Gravity {
+        START, END, LEFT, RIGHT
+    }
+
+    enum class Card(internal val value: Int, @field:DrawableRes internal val drawableRes: Int) {
+        VISA(1, R.drawable.visa),
+        MASTERCARD(2, R.drawable.mastercard),
+        AMEX(4, R.drawable.amex),
+        DISCOVER(8, R.drawable.discover),
+        UNKNOWN(-1, R.drawable.creditcard);
+
+        companion object {
+            internal fun from(@DrawableRes drawableRes: Int): Card {
+                for (card in values()) {
+                    if (card.drawableRes == drawableRes) {
+                        return card
+                    }
+                }
+                return UNKNOWN
+            }
+        }
+    }
+
+    private val textWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {}
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(text: CharSequence, start: Int, lengthBefore: Int, lengthAfter: Int) {
+            val textWithoutSeparator = textWithoutSeparator
+
+            var mDrawableResId = 0
+            for (i in 0 until mCCPatterns.size()) {
+                val key = mCCPatterns.keyAt(i)
+
+                val p = mCCPatterns.get(key)
+
+                val m = p.matcher(textWithoutSeparator)
+                isValidCard = m.find()
+                if (isValidCard) {
+                    mDrawableResId = key
+                    break
+                }
+            }
+            if (mDrawableResId != 0 && mDrawableResId != mCurrentDrawableResId) {
+                mCurrentDrawableResId = mDrawableResId
+            } else if (mDrawableResId == 0) {
+                mCurrentDrawableResId = Card.UNKNOWN.drawableRes
+            }
+            addDrawable()
+            addSeparators()
+        }
+    }
+
+    init {
+        setDisabledCards()
+        inputType = InputType.TYPE_CLASS_PHONE
+        setSeparator(Separator.NONE)
+        setDrawableGravity(Gravity.END)
+        attrs?.let { applyAttributes(it) }
+        addTextChangedListener(textWatcher)
+    }
+
+    private fun applyAttributes(attrs: AttributeSet) {
+        val a = context.theme.obtainStyledAttributes(
+                attrs,
+                R.styleable.EditCredit,
+                0, 0)
+        try {
+            setSeparator(Separator.values()[a.getInt(R.styleable.EditCredit_separator, Separator.NONE.ordinal)])
+            setDisabledCardsInternal(a.getInt(R.styleable.EditCredit_disabledCards, 0))
+            setDrawableGravity(Gravity.values()[a.getInt(R.styleable.EditCredit_drawableGravity, Gravity.END.ordinal)])
+        } finally {
+            a.recycle()
+        }
+    }
+
+    private fun addDrawable() {
+        var currentDrawable = ContextCompat.getDrawable(context, mCurrentDrawableResId)
+        if (currentDrawable != null && TextUtils.isEmpty(error)) {
+            currentDrawable = resize(currentDrawable)
+            when (mDrawableGravity) {
+                EditCredit.Gravity.START -> setDrawablesRelative(start = currentDrawable)
+                EditCredit.Gravity.RIGHT -> setDrawables(right = currentDrawable)
+                EditCredit.Gravity.LEFT -> setDrawables(left = currentDrawable)
+                else -> setDrawablesRelative(end = currentDrawable)
+            }
+        }
+    }
+
+    private fun addSeparators() {
+        val text = text.toString()
+        if (mSeparator != Separator.NONE) {
+            if (text.length > 4 && !text.matches("(?:[0-9]{4}$mSeparator)+[0-9]{1,4}".toRegex())) {
+                val sp = StringBuilder()
+                val caretPosition = selectionEnd
+                val segments = splitString(text.replace(mSeparator.toRegex(), ""))
+                for (segment in segments) {
+                    sp.append(segment).append(mSeparator)
+                }
+                setText("")
+                append(sp.delete(sp.length - mSeparator.length, sp.length).toString())
+                if (caretPosition < text.length)
+                    setSelection(caretPosition)
+            }
+        }
+    }
+
+    private fun removeSeparators() {
+        var text = text.toString()
+        text = text.replace(" ".toRegex(), "").replace("-".toRegex(), "")
+        setText("")
+        append(text)
+    }
+
+    private fun splitString(s: String): Array<String?> {
+        val arrayLength = Math.ceil(s.length / 4.toDouble()).toInt()
+        val result = arrayOfNulls<String>(arrayLength)
+
+        var j = 0
+        val lastIndex = result.size - 1
+        for (i in 0 until lastIndex) {
+            result[i] = s.substring(j, j + 4)
+            j += 4
+        }
+        result[lastIndex] = s.substring(j)
+
+        return result
+    }
+
+    @Deprecated("Please use the method that accepts a Separator enum instead.", ReplaceWith("this.setSeparator(Separator.)"))
+    fun setSeparator(@IntRange(from = 0, to = 2) separator: Int) {
+        if (separator > 2 || separator < 0)
+            throw IllegalArgumentException("The separator has to be one of the following:" +
+                    "NO_SEPARATOR." +
+                    "SPACES_SEPARATOR." +
+                    "DASHES_SEPARATOR.")
+        setSeparator(Separator.values()[separator])
+    }
+
+    /**
+     * Use this method to set the separator style.
+     * The default separator is [Separator.NONE].
+     *
+     * @param separator the style of the separator.
+     */
+    fun setSeparator(separator: Separator) {
+        mSeparator = separator
+        if (mSeparator != Separator.NONE) {
+            filters = arrayOf<InputFilter>(InputFilter.LengthFilter(23))
+            keyListener = DigitsKeyListener.getInstance("0123456789$mSeparator")
+            addSeparators()
+        } else {
+            filters = arrayOf<InputFilter>(InputFilter.LengthFilter(19))
+            keyListener = DigitsKeyListener.getInstance("0123456789")
+            removeSeparators()
+        }
+    }
+
+    /**
+     * Use this method to set the location of the card drawable.
+     * The default gravity is [Gravity.END].
+     *
+     * @param gravity the drawable location.
+     */
+    fun setDrawableGravity(gravity: Gravity) {
+        mDrawableGravity = gravity
+        addDrawable()
+    }
+
+    private fun setDisabledCardsInternal(disabledCards: Int) {
+        val cards = ArrayList<Card>()
+        if (containsFlag(disabledCards, Card.VISA.value)) {
+            cards.add(Card.VISA)
+        }
+        if (containsFlag(disabledCards, Card.MASTERCARD.value)) {
+            cards.add(Card.MASTERCARD)
+        }
+        if (containsFlag(disabledCards, Card.AMEX.value)) {
+            cards.add(Card.AMEX)
+        }
+        if (containsFlag(disabledCards, Card.DISCOVER.value)) {
+            cards.add(Card.DISCOVER)
+        }
+        setDisabledCards(*cards.toTypedArray())
+    }
+
+    @Deprecated("Please use the method that accepts an array of Cards instead.", ReplaceWith("this.setDisabledCards(cards)"))
+    fun setDisabledCards(disabledCards: Int) {
+        setDisabledCardsInternal(disabledCards)
+    }
+
+    /**
+     * Use this method to set which cards are disabled.
+     * By default all supported cards are enabled.
+     *
+     * @param cards the cards to be disabled.
+     */
+    fun setDisabledCards(vararg cards: Card) {
+        var disabledCards = 0
+        for (card in cards) {
+            disabledCards = disabledCards or card.value
+        }
+        mCCPatterns.clear()
+        if (!containsFlag(disabledCards, Card.VISA.value)) {
+            mCCPatterns.put(Card.VISA.drawableRes, Pattern.compile("^4[0-9]{1,12}(?:[0-9]{6})?$"))
+        }
+        if (!containsFlag(disabledCards, Card.MASTERCARD.value)) {
+            mCCPatterns.put(Card.MASTERCARD.drawableRes, Pattern.compile("^5[1-5][0-9]{0,14}$"))
+        }
+        if (!containsFlag(disabledCards, Card.AMEX.value)) {
+            mCCPatterns.put(Card.AMEX.drawableRes, Pattern.compile("^3[47][0-9]{0,13}$"))
+        }
+        if (!containsFlag(disabledCards, Card.DISCOVER.value)) {
+            mCCPatterns.put(Card.DISCOVER.drawableRes, Pattern.compile("^6(?:011|5[0-9]{1,2})[0-9]{0,12}$"))
+        }
+        textWatcher.onTextChanged("", 0, 0, 0)
+    }
+
+    private fun containsFlag(flagSet: Int, flag: Int): Boolean {
+        return flagSet or flag == flagSet
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        var noDrawablesVisible = true
+        for (drawable in compoundDrawables) {
+            if (drawable != null) {
+                noDrawablesVisible = false
+                break
+            }
+        }
+        if (noDrawablesVisible) {
+            addDrawable()
+        }
+    }
+
+    private fun resize(image: Drawable): Drawable? {
+        val imageIntrinsicHeight = image.intrinsicHeight
+        val height = measuredHeight - (paddingTop + paddingBottom)
+        return when {
+            height <= 0 -> null
+            height < imageIntrinsicHeight -> {
+                val b = (image as BitmapDrawable).bitmap
+                val ratio = image.getIntrinsicWidth().toFloat() / imageIntrinsicHeight.toFloat()
+                val bitmapResized = Bitmap.createScaledBitmap(b, (height * ratio).toInt(), height, false)
+                BitmapDrawable(resources, bitmapResized)
+            }
+            else -> image
+        }
+    }
+
+    private fun setDrawablesRelative(start: Drawable? = null, top: Drawable? = null, end: Drawable? = null, bottom: Drawable? = null) =
+            TextViewCompat.setCompoundDrawablesRelativeWithIntrinsicBounds(this, start, top, end, bottom)
+
+    private fun setDrawables(left: Drawable? = null, top: Drawable? = null, right: Drawable? = null, bottom: Drawable? = null) =
+            setCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom)
+
+    companion object {
+        @Deprecated("This constant has been replace with an enum.", ReplaceWith("Separator.NONE"))
+        const val NO_SEPARATOR = 0
+        @Deprecated("This constant has been replace with an enum.", ReplaceWith("Separator.SPACES"))
+        const val SPACES_SEPARATOR = 1
+        @Deprecated("This constant has been replace with an enum.", ReplaceWith("Separator.DASHES"))
+        const val DASHES_SEPARATOR = 2
+
+        @Deprecated("This constant has been replace with an enum.", ReplaceWith("null"))
+        const val NONE = 0
+        @Deprecated("This constant has been replace with an enum.", ReplaceWith("Card.VISA"))
+        const val VISA = 1
+        @Deprecated("This constant has been replace with an enum.", ReplaceWith("Card.MASTERCARD"))
+        const val MASTERCARD = 2
+        @Deprecated("This constant has been replace with an enum.", ReplaceWith("Card.AMEX"))
+        const val AMEX = 4
+        @Deprecated("This constant has been replace with an enum.", ReplaceWith("Card.DISCOVER"))
+        const val DISCOVER = 8
+    }
+}
